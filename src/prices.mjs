2ece0cb00f66cae4804d69279379b83018d289ad
Jeferson Douglas <jefersonmedeiros000@gmail.com>
2024-03-21 23:09:37 -0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -76,7 +76,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && date2 && isMonday(date, date2) && !isHoliday(date, date2)) {
+    if (date && date2 && isMonday(date, date2) && !isHoliday(date2)) {
       reduction = 35;
     }
     return reduction;
@@ -87,7 +87,7 @@ function createApp(database) {
     return date.getDay() === 1;
   }
 
-  function isHoliday(date, date2) {
+  function isHoliday(date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
