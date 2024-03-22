6d9ae6fea4f0b17caa7219e8972f6289f6723ef4
Jeferson Douglas <jefersonmedeiros000@gmail.com>
2024-03-21 22:32:19 -0300

    Creates parsePlainDate funcion
    Signed-off-by: Jeferson Douglas <jefersonmedeiros000@gmail.com>

--- src/prices.mjs
+++ src/prices.mjs
@@ -20,10 +20,12 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
+    const date2 = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
+  function parsePlainDate(dateString) {}
   function parseDate(dateString) {
     if (dateString) {
       return new Date(dateString);
