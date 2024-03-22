ef93e3d69f8ab3e1e7025781e68533186076cbd4
Jeferson Douglas <jefersonmedeiros000@gmail.com>
2024-03-21 22:58:05 -0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -93,7 +93,7 @@ function createApp(database) {
       let holiday = new Date(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
-        date &&
+        date && date2 &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
